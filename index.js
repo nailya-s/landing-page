84d887c449ca33d37e39cfa00a601c4125d31cc7
Nailya Salekhova <salehova.n@gmail.com>
2021-09-27 23:52:08 +0600

    main page

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log('test');
+const btnHamburger = document.querySelector('#btnHamburger');
+const overlay = document.querySelector('.overlay');
 
+
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+  
+    if(btnHamburger.classList.contains('open')){ // Close Hamburger Menu;
+        btnHamburger.classList.remove('open');  
+    }
+    else { // Open Hamburger Menu
+        btnHamburger.classList.add('open');
+    }  
+  });
